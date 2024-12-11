6b455e2715c69ea614f3051b4f5dcae670faf869
hantsy <hantsy@gmail.com>
2024-12-11 18:21:25 +0800

    chore: clean codes

--- concurrency/src/main/java/com/example/GreetingService.java
+++ concurrency/src/main/java/com/example/GreetingService.java
@@ -11,6 +11,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Flow;
 import java.util.concurrent.SubmissionPublisher;
+import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -40,11 +41,39 @@ public class GreetingService {
     }
 
     public Flow.Publisher<GreetingRecord> greetFlow(String name) {
+        class TransformProcessor<S, T> extends SubmissionPublisher<T> implements Flow.Processor<S, T> {
+            final Function<? super S, ? extends T> function;
+            Flow.Subscription subscription;
+
+            TransformProcessor(Executor executor, int maxBufferCapacity, Function<? super S, ? extends T> function) {
+                super(executor, maxBufferCapacity);
+                this.function = function;
+            }
+
+            public void onSubscribe(Flow.Subscription subscription) {
+                (this.subscription = subscription).request(1);
+            }
+
+            public void onNext(S item) {
+                subscription.request(1);
+                submit(function.apply(item));
+            }
+
+            public void onError(Throwable ex) {
+                closeExceptionally(ex);
+            }
+
+            public void onComplete() {
+                close();
+            }
+        }
+
         class LogPublisher extends SubmissionPublisher<GreetingRecord> {
             public LogPublisher(Executor executor, int maxBufferCapacity) {
                 super(executor, maxBufferCapacity);
             }
         }
+
         var publisher = new LogPublisher(executor, 10);
         publisher.submit(new GreetingRecord("Flow", LocalDateTime.now()));
 
