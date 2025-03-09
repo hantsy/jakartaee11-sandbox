4e019458f7412f40da2fb1931f1cfa427ff7eab8
hantsy <hantsy@gmail.com>
2025-03-09 12:48:48 +0800

    chore: clean codes

--- cdi/src/main/java/com/example/ApplicationProperties.java
+++ cdi/src/main/java/com/example/ApplicationProperties.java
@@ -6,13 +6,36 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ConfigProperties(prefix = "app")
 @Dependent
-public record ApplicationProperties(
-        String name,
-        String description,
-        @ConfigProperty(name = "server.host", defaultValue = "localhost")
-        String host
-) {
+public class ApplicationProperties {
+    private String name;
+    private String description;
+    @ConfigProperty(name = "server.host", defaultValue = "localhost")
+    private String host;
 
-        public ApplicationProperties() {
-        }
+    public ApplicationProperties() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
 }
