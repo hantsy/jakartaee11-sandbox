edc73cbcbf8b137eb2365b2d7c31917cffa7fb18
hantsy <hantsy@gmail.com>
2025-03-09 13:16:14 +0800

    chore: clean codes

--- cdi/src/main/java/com/example/ApplicationService.java
+++ cdi/src/main/java/com/example/ApplicationService.java
@@ -1,16 +1,16 @@
 package com.example;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import jakarta.inject.Inject;
 
 @ApplicationScoped
-@RequiredArgsConstructor(onConstructor_ = @Inject)
+@AllArgsConstructor(onConstructor_ = @Inject)
+@NoArgsConstructor
 public record ApplicationService(ApplicationProperties properties) {
 
-    public ApplicationService() {
-        this(null);
-    }
     
     public String hello(String name) {
         return "Hello " + name + " from " + properties.getName();
