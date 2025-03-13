79ee12dddba0b560979fcf31ee0902e4b7f9f29f
hantsy <hantsy@gmail.com>
2025-03-13 17:13:09 +0800

    chore: clean codes

--- data/src/main/java/library/common/Logged.java
+++ data/src/main/java/library/common/Logged.java
@@ -1,9 +1,6 @@
 package library.common;
 
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Stereotype;
 import jakarta.interceptor.InterceptorBinding;
-import jakarta.transaction.Transactional;
 
 import java.lang.annotation.*;
 
